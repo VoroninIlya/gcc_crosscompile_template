22dfddd84f4fec7a1e9ed3305ca0d309bc359f28
Ilia Voronin <woronin.ilja@gmail.com>
2025-03-11 21:15:33 +0100

    Added linux preset to develop and debug in vs code through ssh

---
+++ cmake/gcc.cmake
@@ -0,0 +1,7 @@
+set(CMAKE_SYSTEM_NAME               Linux)
+
+# Some default GCC settings
+set(CMAKE_C_COMPILER                gcc)
+set(CMAKE_CXX_COMPILER              g++)
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
